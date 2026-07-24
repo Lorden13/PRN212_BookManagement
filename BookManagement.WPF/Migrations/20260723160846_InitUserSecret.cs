7b9b5e4a73ae49c703f98eedb59b519b7c830075
TaiXN <thientai1109@gmail.com>
2026-07-24 07:09:52 +0700

    User Secret

---
+++ BookManagement.WPF/Migrations/20260723160846_InitUserSecret.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitUserSecret : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SavedToken",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    TokenValue = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SavedToken", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SavedToken");
+        }
+    }
+}
